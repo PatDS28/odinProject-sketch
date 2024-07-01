95ec5fd2d8bb68ee2b30d6319540e99223c75427
PatDS28 <johnpatrickdesalit@gmail.com>
2024-07-01 16:52:22 +0800

    project finished, added button to customize grid upon prompt, grid fixed with hover function

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 
-
-// for(let i = 1;i<16;i++){
-//   const div = document.createElement("div");
-//   for(let j = 1; j<16;j++){
-//     const div = document.createElement("div");
-//   }
-// }
-
-const div = document.createElement("div");
 const mainDiv = document.querySelector(".main");
 
-for(var i = 1;i<=16;i++){
-  console.log(i);
-  const div = document.createElement("div");
-  div.classList.add(`div-${i}`);
-  mainDiv.appendChild(div);
-  for(var j = 1; j<=15;j++){
-    console.log(j);
-    const div = document.createElement("div");
-    div.classList.add(`div-${i}-${j}`);
-    mainDiv.appendChild(div);
-    
+function hoverEvent(buttons){
+  buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("mouseover", () => {
+      button.style.backgroundColor = "red";
+    });
+    button.addEventListener("mouseout", () => {
+      // button.style.backgroundColor = "white";
+    });
+  });
+  
+}
+
+
+function removeAllItems(container){
+  while(container.firstChild){
+    container.removeChild(container.firstChild)
   }
 }
 
 
-// mainDiv.appendChild(div);
+const buttons = document.querySelectorAll(".main div");
+const customizeButton = document.querySelector(".customize");
+customizeButton.addEventListener("click",function (e){
+  removeAllItems(mainDiv);
+
+  do{
+    var gridCount = parseInt(prompt("Type the number of grid n x n"));
+  }while(!(gridCount>0 && gridCount<=100));
+  
+  for(var i = 1;i<=gridCount;i++){
+    const div = document.createElement("div");
+    div.classList.add(`div-${i}`);
+    div.setAttribute("style",`width: calc(100%/${gridCount}); height: calc(100%/${gridCount});`)
+    mainDiv.appendChild(div);
+    for(var j = 1; j<=gridCount;j++){
+      const div = document.createElement("div");
+      div.classList.add(`div-${i}-${j}`);
+      div.setAttribute("style",`width: calc(100%/${gridCount}); height: calc(100%/${gridCount});`)
+      mainDiv.appendChild(div);
+    }
+  }
+  const newDiv = document.querySelectorAll(".main Div");
+  hoverEvent(newDiv);
+});
